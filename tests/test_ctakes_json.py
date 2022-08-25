73870ccbb3974f10647d15f3b1ab1abec463ff73
comorbidity <andrew.mcmurry@tch.harvard.edu>
2022-08-25 13:51:04 -0700

    organized imports.

--- tests/test_ctakes_json.py
+++ tests/test_ctakes_json.py
@@ -1,20 +1,24 @@
 import unittest
-from etl import store
-from etl.ctakes.ctakes_json import CtakesJSON
+import os
+import json
+
+from ctakes.ctakes_json import CtakesJSON
 
 class TestCtakesJSON(unittest.TestCase):
 
-    def test_parse_json(self, example='/your/path/to/ctakes.json'):
+    def test_parse_json(self, example_json='/your/path/to/ctakes.json'):
 
-        example = '/Users/andy/phi/i2b2/processed/1069/106913362/e5830e63ee070e734d5c0b2e66a5f19e/ctakes.json'
+        if os.path.exists(example_json):
+            with open(example_json, 'r') as f:
+                from_json = json.load(f)
 
-        if store.path_exists(example):
-            from_json = store.read_json(example)
-            reader = CtakesJSON(from_json)
+                reader = CtakesJSON(from_json)
 
-            self.assertDictEqual(from_json, reader.as_json(), 'ctakes json did not match before/after serialization')
+                self.assertDictEqual(from_json, reader.as_json(), 'ctakes json did not match before/after serialization')
 
-            self.assertGreaterEqual(len(reader.list_match()), 1, 'response should have at least one match')
-            self.assertGreaterEqual(len(reader.list_match_text()), 1, 'response should have at least one text match')
-            self.assertGreaterEqual(len(reader.list_concept()), 1, 'response should have at least one concept')
-            self.assertGreaterEqual(len(reader.list_concept_cui()), 1, 'response should have at least one concept CUI')
+                self.assertGreaterEqual(len(reader.list_match()), 1, 'response should have at least one match')
+                self.assertGreaterEqual(len(reader.list_match_text()), 1, 'response should have at least one text match')
+                self.assertGreaterEqual(len(reader.list_concept()), 1, 'response should have at least one concept')
+                self.assertGreaterEqual(len(reader.list_concept_cui()), 1, 'response should have at least one concept CUI')
+        else:
+            print(f'skipping test, example_json does not exist: {example_json}')
