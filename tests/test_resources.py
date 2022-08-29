2043dc1c024a5a32ff77cc6d0c1e2f06e808b90f
comorbidity <andrew.mcmurry@tch.harvard.edu>
2022-08-29 14:46:46 -0700

    ctakes JSON TEST

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,3 +1,9 @@
 import os
+import unittest
 
 COVID_SYMPTOMS_BSV = os.path.join(os.getcwd(), '../resources/covid_symptoms.bsv')
+
+class TestResources(unittest.TestCase):
+
+    def test_exists_resource_file(self, resource_file=COVID_SYMPTOMS_BSV):
+        self.assertTrue(os.path.exists(resource_file), f'resource did not exist: {resource_file}')
