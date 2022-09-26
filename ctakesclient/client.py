a5dbd214065719aff9baad4260fdc5c4ffb9be91
comorbidity <andrew.mcmurry@tch.harvard.edu>
2022-09-26 10:30:53 -0400

    client.py includes optional hints for UTF-8 and ASCII encoding. NO Assumption is made of the user as to which is preferred.

--- ctakesclient/client.py
+++ ctakesclient/client.py
@@ -24,12 +24,29 @@ def get_url_ctakes_rest() -> str:
         'URL_CTAKES_REST',
         'http://localhost:8080/ctakes-web-rest/service/analyze')
 
+def ascii(sentence:str) -> bytes:
+    """
+    https://pythonguides.com/remove-unicode-characters-in-python/#:~:text=In%20python%2C%20to%20remove%20non,decode().
+    :param sentence:
+    :return: sentence REMOVED of unicode chars - note LENGTH of text input may change!
+    """
+    return sentence.encode("ascii", "ignore")
+
+def utf8(sentence:str) -> bytes:
+    """
+    https://stackoverflow.com/questions/34618149/post-unicode-string-to-web-service-using-python-requests-library
+    :param sentence:
+    :return: sentence ESCAPED unicode chars - note LENGTH of text input may change!
+    """
+    # import re
+    # return re.sub(r'[^\u1F600-\u1F64F ]|[^\u1F300-\u1F5FF ]', " ", sentence)
+    return sentence.encode("utf-8")
 
 def post(sentence: str, url=get_url_ctakes_rest()) -> dict:
     """
     :param sentence: clinical text to send to cTAKES
     :param url: cTAKES REST server fully qualified path
-    :return:
+    :return: dict JSON response from cTAKES
     """
     logging.debug(url)
     # TODO: consider exposing a pass-through timeout parameter
